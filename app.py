cf1daaea6d859fe6976650d4da87710a8927180b
Siroj <sirojkalandarov019@icloud.com>
2025-05-28 17:29:34 +0500

    ✅ Добавил маршрут /webapp

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('profile.html')
+    return render_template('index.html')  # если index.html — твоя главная
+
+@app.route('/webapp')  # 👈 вот это добавь
+def webapp():
+    return render_template('profile.html')  # или webapp.html, как хочешь
 
 if __name__ == '__main__':
     app.run(debug=True)
