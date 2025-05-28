1aca8bbbcb66a33d3feaa5519c5c0301a6856738
Siroj <sirojkalandarov019@icloud.com>
2025-05-28 22:05:23 +0500

    💡 Улучшен вид и логика страницы профиля

--- app.py
+++ app.py
@@ -12,3 +12,38 @@ def webapp():
 
 if __name__ == '__main__':
     app.run(debug=True)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Стартовая страница
+
+@app.route('/webapp')
+def webapp():
+    return render_template('profileuser.html')  # Первая форма
+
+@app.route('/main')
+def main_menu():
+    return render_template('main_menu.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/wallet')
+def wallet():
+    return render_template('wallet.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
