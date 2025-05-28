f7383959c1a1567714f2505db1895591b1d4a8c8
Siroj <sirojkalandarov019@icloud.com>
2025-05-28 15:45:01 +0500

    добавил маршрут WebApp

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/webapp')
+def webapp():
+    return render_template('webapp.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
